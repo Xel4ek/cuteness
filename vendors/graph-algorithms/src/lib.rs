61a96a707c4ac45cf56f08e968211078acdb7a5a
Xel4ek <a.itskovich@iva.ru>
2024-12-07 13:46:18 +0300

    update

--- vendors/graph-algorithms/src/lib.rs
+++ vendors/graph-algorithms/src/lib.rs
@@ -5,6 +5,8 @@ mod path_restore;
 mod redux;
 mod transform;
 
+use wasm_bindgen::prelude::*;
+
 #[cfg(target_arch = "wasm32")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
